20d81aaf263ea05081562113ece2f9f54435aac2
sue2gakupo <sue2gakupo@gmail.com>
2025-05-19 16:35:53 +0800

    add ErrorMessage

--- MyView/Models/NightMarket.cs
+++ MyView/Models/NightMarket.cs
@@ -1,10 +1,21 @@
-﻿namespace MyView.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyView.Models
 {
     public class NightMarket
     {
-
+        [Display(Name="編號")]
+        [Required(ErrorMessage ="必填")]
+        [RegularExpression("A[0-9]{2}",ErrorMessage ="編號格式有誤")]
         public string Id { get; set; }=null!;
+
+        [Display(Name = "夜市名稱")]
+        [Required(ErrorMessage = "必填")]
+        [StringLength(20,ErrorMessage ="最長20個字")]
         public string Name { get; set; } = null!;
+
+        [Display(Name = "地址")]
+        [Required(ErrorMessage = "必填")]
         public string Address { get; set; } = null!;
 
     }
