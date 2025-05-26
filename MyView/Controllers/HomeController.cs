911b364c8cdd0d098eecf388562e9ef490e13e5d
SuuYii <sue2gakupo@gmail.com>
2025-05-26 16:30:17 +0800

    feat(admin): add Details.cshtml and update LINQ/Lambda to display night market details in admin panel

--- MyView/Controllers/HomeController.cs
+++ MyView/Controllers/HomeController.cs
@@ -14,7 +14,7 @@ namespace MyView.Controllers
             _logger = logger;
         }
 
-        public IActionResult IndexRWD()
+        private static List<NightMarket> GetData()
         {
             string[] id = { "A01", "A02", "A03", "A04", "A05", "A06", "A07" };
             string[] name = { "瑞豐夜市", "新堀江商圈", "六合夜市", "青年夜市", "花園夜市", "大東夜市", "武聖夜市" };
@@ -31,49 +31,73 @@ namespace MyView.Controllers
                 nm.Name = name[i];
                 nm.Address = address[i];
                 list.Add(nm);
-
             }
 
+            return list;
+        }
 
-            return View(list);
+        public IActionResult IndexRWD()
+        {
+            return View(GetData());
+        }
+
+        public IActionResult IndexList(string id)
+        {
+            var list = GetData();
+
+
+            VMNightMarket vMN = new VMNightMarket()
+            {
+                //左側導覽列
+                //取得所有夜市資料的編號與名稱
+                NightMarkets = list,
+                //右側顯示資料內容主畫面
+                //取得某一筆夜市資料的詳細內容
+                NightMarket = list.Where(list => list.Id == id).FirstOrDefault()
+            };
+
+           
+
+            //左側導覽列
+            //取得所有夜市資料的編號與名稱
+            //ViewBag["nm"] = list;
+
+            //右側顯示資料內容主畫面
+            //取得某一筆夜市資料的詳細內容
+            //Lambda寫法
+
+            //var result =list.Where(list => list.Id == id).FirstOrDefault();
+
+
+            return View(vMN);
         }
 
         public IActionResult Index()
         {
-            //用陣列存放資料
 
-            string[] id = { "A01", "A02", "A03", "A04", "A05", "A06", "A07" };
-            string[] name = { "瑞豐夜市", "新堀江商圈", "六合夜市", "青年夜市", "花園夜市", "大東夜市", "武聖夜市" };
-            string[] address = { "813高雄市左營區裕誠路", "800高雄市新興區玉衡里", "800台灣高雄市新興區六合二路",
-                "80652高雄市前鎮區凱旋四路758號", "台南市北區海安路三段533號", "台南市東區林森路一段276號",
-                "台南市中西區武聖路69巷42號" };
-
-            //鑄造物件，程式碼一樣，放進for迴圈
-            //NightMarket nm=new NightMarket();
-            //nm.Id = id[0];
-            //nm.Name = name[0];
-            //nm.Address = address[0];
-
-            //NightMarket nm2 = new NightMarket();
-            //nm2.Id = id[1];
-            //nm2.Name = name[1];
-            //nm2.Address = address[1];
-
-            List<NightMarket> list = new List<NightMarket>();
-
-            for (int i = 0; i < id.Length; i++)
-            {
-                NightMarket nm = new NightMarket();
-                nm.Id = id[i];
-                nm.Name = name[i];
-                nm.Address = address[i];
-                list.Add(nm);
-
-            }
-
-
-            return View(list);
+            return View(GetData());
         }
+
+        public IActionResult Details(string id)
+        {
+            var list = GetData();
+
+            //select *
+            //from NightMarket(=list
+            //where Id = 'A09'
+
+            ////Linq寫法:接近SQL語法
+            //var result = (from n in list
+            //             where n.Id == id
+            //             select n).FirstOrDefault();
+
+            //Lambda寫法:簡潔，可先寫Linq藉助寫出來
+            var result = list.Where(list => list.Id == id).FirstOrDefault();
+
+
+            return View(result);
+        }
+
 
         public IActionResult Create()
         {
---
+++ MyView/Models/VMNightMarket.cs
@@ -0,0 +1,8 @@
+﻿namespace MyView.Models
+{
+    public class VMNightMarket
+    {
+        public List<NightMarket>NightMarkets { get; set; } = null!;
+        public NightMarket NightMarket { get; set; } = null!;
+    }
+}
